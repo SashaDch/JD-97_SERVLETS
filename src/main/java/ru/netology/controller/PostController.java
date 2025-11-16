cc9016de844ceaab0e5560df290b64cd5c01b859
SashaDch <sashadychko@mail.ru>
2025-11-16 17:22:34 +0300

    autoformatted

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -11,40 +11,40 @@ import java.io.Reader;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
-  private final AtomicLong counter =  new AtomicLong();
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final AtomicLong counter = new AtomicLong();
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
-
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
-
-  public void getById(long id, HttpServletResponse response) throws IOException, NotFoundException {
-      response.setContentType(APPLICATION_JSON);
-      final var data = service.getById(id);
-      final var gson = new Gson();
-      response.getWriter().print(gson.toJson(data));
-  }
-
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    if (post.getId() == 0L) {
-        post.setId(counter.incrementAndGet());
+    public PostController(PostService service) {
+        this.service = service;
     }
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
 
-  public void removeById(long id, HttpServletResponse response) throws NotFoundException {
-      service.removeById(id);
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException, NotFoundException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.getById(id);
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        if (post.getId() == 0L) {
+            post.setId(counter.incrementAndGet());
+        }
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws NotFoundException {
+        service.removeById(id);
+    }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -10,67 +10,69 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
-  private enum Method {
+    private PostController controller;
+
+    private enum Method {
         GET,
         POST,
         PUT,
         DELETE
-  }
-  private final String POSTS_ROOT = "/api/posts";
-
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    try {
-      final String path = req.getRequestURI();
-      final Method method = Method.valueOf(req.getMethod());
-      if (!path.matches(POSTS_ROOT + "(/\\d+)?")) {
-          resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-          return;
-      }
-      final long id = (path.matches(POSTS_ROOT + "/\\d+")) ?
-              Long.parseLong(path.substring(path.lastIndexOf("/") + 1)) : 0;
-      switch (method) {
-          case GET -> {
-              if (path.equals(POSTS_ROOT)) {
-                  controller.all(resp);
-              } else {
-                  controller.getById(id, resp);
-              }
-          }
-          case POST, PUT -> {
-              synchronized (controller) {
-              if (method == Method.PUT || id != 0L) {
-                  controller.removeById(id, resp);
-              }
-              controller.save(req.getReader(), resp);
-              }
-              if (id == 0L) {
-                  resp.setStatus(HttpServletResponse.SC_CREATED);
-              } else {
-                  resp.setStatus(HttpServletResponse.SC_ACCEPTED);
-              }
-          }
-          case DELETE -> {
-              controller.removeById(id, resp);
-              resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-          }
-      }
-    } catch (IllegalArgumentException e) {
-        resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-    } catch (NotFoundException e) {
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
-  }
+
+    private final String POSTS_ROOT = "/api/posts";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final String path = req.getRequestURI();
+            final Method method = Method.valueOf(req.getMethod());
+            if (!path.matches(POSTS_ROOT + "(/\\d+)?")) {
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+            final long id = (path.matches(POSTS_ROOT + "/\\d+")) ?
+                    Long.parseLong(path.substring(path.lastIndexOf("/") + 1)) : 0;
+            switch (method) {
+                case GET -> {
+                    if (path.equals(POSTS_ROOT)) {
+                        controller.all(resp);
+                    } else {
+                        controller.getById(id, resp);
+                    }
+                }
+                case POST, PUT -> {
+                    synchronized (controller) {
+                        if (method == Method.PUT || id != 0L) {
+                            controller.removeById(id, resp);
+                        }
+                        controller.save(req.getReader(), resp);
+                    }
+                    if (id == 0L) {
+                        resp.setStatus(HttpServletResponse.SC_CREATED);
+                    } else {
+                        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+                    }
+                }
+                case DELETE -> {
+                    controller.removeById(id, resp);
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        } catch (NotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
