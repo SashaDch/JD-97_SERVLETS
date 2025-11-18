3f4927c9de414cbb1b26576b82576e60f590c827
SashaDch <sashadychko@mail.ru>
2025-11-18 21:11:05 +0300

    spring di with java config

---
+++ src/main/java/ru/netology/config/Config.java
@@ -0,0 +1,25 @@
+package ru.netology.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+@Configuration
+public class Config {
+    @Bean
+    PostController postController() {
+        return new PostController(postService());
+    }
+
+    @Bean
+    PostService postService() {
+        return new PostService(postRepository());
+    }
+
+    @Bean
+    PostRepository postRepository() {
+        return new PostRepository();
+    }
+}
